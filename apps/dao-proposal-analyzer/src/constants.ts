22169f553119c24b480fa987b0738ad45b871cb0
SidharthK2 <sidharthkapoor.100@gmail.com>
2026-01-15 18:49:55 +0530

    add snapshot dao links

--- apps/dao-proposal-analyzer/src/constants.ts
+++ apps/dao-proposal-analyzer/src/constants.ts
@@ -25,6 +25,8 @@ export const SUPPORTED_DAOS = {
 		governor: "0xc0Da02939E1441F497fd74F78cE7Decb17B66529",
 		token: "0xc00e94Cb662C3520282E6f5717214004A7f26888",
 		chain: "Ethereum",
+		proposalUrl: (id: string) =>
+			`https://compound.finance/governance/proposals/${id}`,
 	},
 	// Uniswap Governor Bravo
 	UNISWAP: {
@@ -32,6 +34,8 @@ export const SUPPORTED_DAOS = {
 		governor: "0x408ED6354d4973f66138C91495F2f2FCbd8724C3",
 		token: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
 		chain: "Ethereum",
+		proposalUrl: (id: string) =>
+			`https://app.uniswap.org/vote/${id}`,
 	},
 	// ENS Governor
 	ENS: {
@@ -39,6 +43,8 @@ export const SUPPORTED_DAOS = {
 		governor: "0x323A76393544d5ecca80cd6ef2A560C6a395b7E3",
 		token: "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72",
 		chain: "Ethereum",
+		proposalUrl: (id: string) =>
+			`https://www.tally.xyz/gov/ens/proposal/${id}`,
 	},
 	// Aave Governance V3
 	AAVE: {
@@ -46,6 +52,8 @@ export const SUPPORTED_DAOS = {
 		governor: "0x9AEE0B04504CeF83A65AC3f0e838D0593BCb2BC7",
 		token: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
 		chain: "Ethereum",
+		proposalUrl: (id: string) =>
+			`https://app.aave.com/governance/v3/proposal/?proposalId=${id}`,
 	},
 } as const;
 
